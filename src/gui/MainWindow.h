298d8c70fb34a1e60a9be29d57e8bafeee0efc70
Francois Ferrand <thetypz@gmail.com>
2013-04-29 18:04:26 +0200

    Use search field for search.
    Search options are presented in a context menu on the search field, as well as
    links in search header. A 'header' is still displayed while in search mode, to
    clearly indicate the mode.
    
    This provides a slightly simpler (no extra click), more common UI, while keeping
    the all the features of the earlier search mode.

--- src/gui/MainWindow.h
+++ src/gui/MainWindow.h
@@ -60,6 +60,7 @@ private Q_SLOTS:
     void rememberOpenDatabases(const QString& filePath);
 
 private:
+    void updateSearchField(DatabaseWidget* dbWidget = NULL);
     static void setShortcut(QAction* action, QKeySequence::StandardKey standard, int fallback = 0);
 
     static const QString BaseWindowTitle;
