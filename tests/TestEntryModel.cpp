ca77268b925df0acd154c909b27d544c3cb9def2
Didier Fabert <didier.fabert@gmail.com>
2014-03-29 17:22:00 +0100

    Update test for entrymodel

--- tests/TestEntryModel.cpp
+++ tests/TestEntryModel.cpp
@@ -282,7 +282,7 @@ void TestEntryModel::testProxyModel()
 
     QSignalSpy spyColumnRemove(modelProxy, SIGNAL(columnsAboutToBeRemoved(QModelIndex,int,int)));
     modelProxy->hideColumn(0, true);
-    QCOMPARE(modelProxy->columnCount(), 3);
+    QCOMPARE(modelProxy->columnCount(), 4);
     QVERIFY(spyColumnRemove.size() >= 1);
 
     int oldSpyColumnRemoveSize = spyColumnRemove.size();
@@ -298,7 +298,7 @@ void TestEntryModel::testProxyModel()
 
     QSignalSpy spyColumnInsert(modelProxy, SIGNAL(columnsAboutToBeInserted(QModelIndex,int,int)));
     modelProxy->hideColumn(0, false);
-    QCOMPARE(modelProxy->columnCount(), 4);
+    QCOMPARE(modelProxy->columnCount(), 5);
     QVERIFY(spyColumnInsert.size() >= 1);
 
     int oldSpyColumnInsertSize = spyColumnInsert.size();
