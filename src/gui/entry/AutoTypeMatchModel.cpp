444f2f16d579ace6838dde30b31bb4f189a2f3f9
David Wu <lightvector@gmail.com>
2015-11-01 22:48:01 -0500

    Fix after merge, change new file copyright headers

--- src/gui/entry/AutoTypeMatchModel.cpp
+++ src/gui/entry/AutoTypeMatchModel.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2010 Felix Geyer <debfx@fobos.de>
+ *  Copyright (C) 2015 David Wu <lightvector@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -192,7 +192,7 @@ void AutoTypeMatchModel::entryRemoved()
 void AutoTypeMatchModel::severConnections()
 {
     Q_FOREACH (const Group* group, m_allGroups) {
-        disconnect(group, Q_NULLPTR, this, Q_NULLPTR);
+        disconnect(group, nullptr, this, nullptr);
     }
 }
 
